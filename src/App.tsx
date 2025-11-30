6cbc6455486c662682a473df78e16b4a70bf94b8
kevin Liu <kevin07liu@gmail.com>
2025-11-30 19:51:46 +0800

    add useAppState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import "./App.css";
-import { Cover } from "./Page/Cover";
+import { Page } from "./Page/Page";
 function App() {
   return (
     <>
-      <Cover />
+      <Page />
     </>
   );
 }
