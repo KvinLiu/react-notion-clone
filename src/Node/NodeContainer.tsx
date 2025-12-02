f95f4e43904fbe9e6ad6abfdde84c55e00b7a16d
kevin Liu <kevin07liu@gmail.com>
2025-12-02 16:18:01 +0800

    add NodeContainer

---
+++ src/Node/NodeContainer.tsx
@@ -0,0 +1,44 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useAppState } from "../state/AppStateContext";
+import { NodeTypeSwitcher } from "./NodeTypeSwitcher";
+import styles from "./NodeContainer.module.css";
+type NodeContainerProps = {
+  node: NodeData;
+  index: number;
+  updateFocusedIndex(index: number): void;
+  isFocused: boolean;
+};
+
+export const NodeContainer = ({
+  node,
+  index,
+  isFocused,
+  updateFocusedIndex,
+}: NodeContainerProps) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: node.id });
+  const { changeNodeValue, changeNodeType, removeNodeByIndex } = useAppState();
+  const { type, value } = node;
+  const style = {
+    transform: CSS.Transform.toString(transform),
+  };
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      className={styles.container}
+    >
+      <div {...listeners} className={styles.dragHandle}>
+        ⠿
+      </div>
+      <NodeTypeSwitcher
+        node={node}
+        index={index}
+        isFocused={isFocused}
+        updateFocusedIndex={updateFocusedIndex}
+      />
+    </div>
+  );
+};
